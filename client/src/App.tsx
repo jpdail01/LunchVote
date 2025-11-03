f09e6ab0f856ecbdcafbc2341923219fa7fa5f21
Jason Dailey <Jason.Dailey@eblusolutions.com>
2025-11-03 17:29:12 -0500

    put base back in vite

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router, useLocation } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -7,7 +7,7 @@ import { ThemeProvider } from "@/hooks/use-theme";
 import HomePage from "@/pages/HomePage";
 import NotFound from "@/pages/not-found";
 
-function Router() {
+function AppRouter() {
   return (
     <Switch>
       <Route path="/" component={HomePage} />
@@ -22,7 +22,9 @@ function App() {
       <ThemeProvider>
         <TooltipProvider>
           <Toaster />
-          <Router />
+          <Router hook={useLocation}>
+            <AppRouter />
+          </Router>
         </TooltipProvider>
       </ThemeProvider>
     </QueryClientProvider>
